5af9f173bb5265f6d6f69e30cf157e5119f81e54
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-15 11:48:55 +0300

    added functions project

---
+++ AzureGameRoomsScaler/CommonConstants.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+
+namespace AzureGameRoomsScaler
+{
+    public class AzureMgmtCredentials
+    {
+        public const string ManagementURI = "https://management.core.windows.net/";
+        public const string BaseURL = "https://management.azure.com/";
+        public const string AuthURL = "https://login.windows.net/";
+        public const string GraphURL = "https://graph.windows.net/";
+        public string SubscriptionId { get; private set; }
+        public string ClientId { get; private set; }
+        public string ClientKey { get; private set; }
+        public string Tenant { get; private set; }
+
+        public AzureMgmtCredentials()
+        {
+            SubscriptionId = ConfigurationManager.AppSettings["SubscriptionId"].ToString();
+            ClientId = ConfigurationManager.AppSettings["ClientId"].ToString();
+            ClientKey = ConfigurationManager.AppSettings["ClientKey"].ToString();
+            Tenant = ConfigurationManager.AppSettings["Tenant"].ToString();
+        }
+
+        public static readonly AzureMgmtCredentials instance = new AzureMgmtCredentials();
+    }
+}
---
+++ AzureGameRoomsScaler/CreateNode.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace AzureGameRoomsScaler
+{
+    public struct NodeParameters
+    {
+        // required
+        public string Region { get; set; }
+        // optional
+        public string Image { get; set; }
+        // required
+        public string Size { get; set; }
+    }
+    public static class CreateNode
+    {
+        [FunctionName("Function1")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "node/create")]HttpRequestMessage req, TraceWriter log)
+        {
+            var content = await req.Content.ReadAsStringAsync();
+            var nodeParams = JsonConvert.DeserializeObject<NodeParameters>(content);
+
+            if (string.IsNullOrEmpty(nodeParams.Size) || string.IsNullOrEmpty(nodeParams.Region))
+            {
+                return req.CreateErrorResponse(HttpStatusCode.BadRequest, new System.ArgumentException("Node parameters <region, size> are required"));
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
