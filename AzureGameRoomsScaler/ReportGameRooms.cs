6fc1a7818565687aab0aa72df8b8c1a23a781773
Ivan Fateev <ivan.fateev@microsoft.com>
2018-01-22 12:01:49 +0300

    added stub with logs for reporting game rooms

--- AzureGameRoomsScaler/ReportGameRooms.cs
+++ AzureGameRoomsScaler/ReportGameRooms.cs
@@ -14,9 +14,29 @@ namespace AzureGameRoomsScaler
 
     public static class ReportGameRooms
     {
-        [FunctionName("ReportGameRooms")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "report/gamerooms")]HttpRequestMessage req, TraceWriter log)
+        public struct NodeRooms
         {
+            public string nodeId { get; set; }
+            public int rooms { get; set; }
+        }
+        public struct Request
+        {
+            public NodeRooms[] nodes { get; set; }
+        }
+        [FunctionName("ReportGameRooms")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "report/gamerooms")]HttpRequestMessage req, TraceWriter log)
+        {
+            var nodes = JsonConvert.DeserializeObject<Request>(await req.Content.ReadAsStringAsync());
+            if (nodes.nodes == null || nodes.nodes.Length == 0)
+            {
+                return req.CreateErrorResponse(HttpStatusCode.BadRequest, "Nodes payload not found.");
+            }
+
+            foreach (var node in nodes.nodes)
+            {
+                log.Info($"Reported {node.nodeId} has {node.rooms} rooms");
+            }
+
             return req.CreateResponse(HttpStatusCode.OK);
         }
     }
