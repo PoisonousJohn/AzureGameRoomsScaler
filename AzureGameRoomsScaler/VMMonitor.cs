a7f08a1089871a0f1339707c4e1dc8c214375a09
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-01-22 15:10:21 +0200

    debugging info for VM Creation

--- AzureGameRoomsScaler/VMMonitor.cs
+++ AzureGameRoomsScaler/VMMonitor.cs
@@ -12,38 +12,38 @@ using Newtonsoft.Json;
 namespace AzureGameRoomsScaler
 {
 
-	public static class VMMonitor
-	{
-		[FunctionName("VMMonitor")]
-		public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "VMMonitor")]HttpRequestMessage req, TraceWriter log)
-		{
-			// Get request body
-			dynamic dataobject = await req.Content.ReadAsAsync<object>();
-			//log.Info(dataobject.ToString());
-
-			var activityLog = dataobject.data.context.activityLog;
-			//log.Info(activityLog.ToString());
-			if (activityLog.operationName == "Microsoft.Compute/virtualMachines/write")
-			{
-				if (activityLog.subStatus == "")
-					log.Info("VM creating");
-				else if (activityLog.subStatus == "Created")
-					log.Info("VM created");
-			}
-			else if (activityLog.operationName == "Microsoft.Compute/virtualMachines/restart/action" && activityLog.status == "Succeeded")
-			{
-				log.Info("VM rebooted");
-			}
-			else if (activityLog.operationName == "Microsoft.Compute/virtualMachines/deallocate/action" && activityLog.status == "Succeeded")
-			{
-				log.Info("VM deallocated");
-			}
-			else if (activityLog.operationName == "Microsoft.Compute/virtualMachines/start/action" && activityLog.status == "Succeeded")
-			{
-				log.Info("VM started");
-			}
-
-			return req.CreateResponse(HttpStatusCode.OK, "Hello");
-		}
-	}
+    public static class VMMonitor
+    {
+        [FunctionName("VMMonitor")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "VMMonitor")]HttpRequestMessage req, TraceWriter log)
+        {
+            // Get request body
+            dynamic dataobject = await req.Content.ReadAsAsync<object>();
+            //log.Info(dataobject.ToString());
+            log.Info("----------------------------------------------");
+            var activityLog = dataobject.data.context.activityLog;
+            log.Info(activityLog.ToString());
+            if (activityLog.operationName == "Microsoft.Compute/virtualMachines/write")
+            {
+                if (activityLog.subStatus == "")
+                    log.Info("VM creating");
+                else if (activityLog.subStatus == "Created")
+                    log.Info("VM created");
+            }
+            else if (activityLog.operationName == "Microsoft.Compute/virtualMachines/restart/action" && activityLog.status == "Succeeded")
+            {
+                log.Info("VM rebooted");
+            }
+            else if (activityLog.operationName == "Microsoft.Compute/virtualMachines/deallocate/action" && activityLog.status == "Succeeded")
+            {
+                log.Info("VM deallocated");
+            }
+            else if (activityLog.operationName == "Microsoft.Compute/virtualMachines/start/action" && activityLog.status == "Succeeded")
+            {
+                log.Info("VM started");
+            }
+            log.Info("----------------------------------------------");
+            return req.CreateResponse(HttpStatusCode.OK, "Hello");
+        }
+    }
 }
