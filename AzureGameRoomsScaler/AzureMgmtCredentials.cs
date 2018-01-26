df2dfef2c1b27551ac0ca729ab8b8823dd14d1cd
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-01-26 22:36:02 +0200

    added method to get public IP

--- AzureGameRoomsScaler/AzureMgmtCredentials.cs
+++ AzureGameRoomsScaler/AzureMgmtCredentials.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Network.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
 using System.Configuration;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace AzureGameRoomsScaler
 {
@@ -10,14 +13,16 @@ namespace AzureGameRoomsScaler
     {
         public IAzure Azure { get; private set; }
 
+        private string subscriptionId;
+        private AzureCredentials credentials;
 
         public AzureMgmtCredentials()
         {
-            var subscriptionId = ConfigurationManager.AppSettings["SubscriptionId"].ToString();
+            subscriptionId = ConfigurationManager.AppSettings["SubscriptionId"].ToString();
             var clientId = ConfigurationManager.AppSettings["ClientId"].ToString();
             var clientSecret = ConfigurationManager.AppSettings["ClientSecret"].ToString();
             var tenant = ConfigurationManager.AppSettings["Tenant"].ToString();
-            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(
+            credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(
                 clientId, clientSecret, tenant, AzureEnvironment.AzureGlobalCloud
             );
             Azure = Microsoft.Azure.Management.Fluent.Azure
@@ -28,5 +33,26 @@ namespace AzureGameRoomsScaler
         }
 
         public static readonly AzureMgmtCredentials instance = new AzureMgmtCredentials();
+
+
+        public async Task<string> GetVMPublicIP(string VMID, string resourceGroup)
+        {
+            var vm = await Azure.VirtualMachines.GetByResourceGroupAsync(resourceGroup, VMID);
+
+            using (var client = new NetworkManagementClient(credentials))
+            {
+                string networkInterfaceId = vm.NetworkInterfaceIds[0].Split('/').Last();
+                client.SubscriptionId = subscriptionId;
+                var network =
+                    await NetworkInterfacesOperationsExtensions.GetAsync(client.NetworkInterfaces,
+                    resourceGroup, networkInterfaceId);
+                string publicIPResourceId = network.IpConfigurations[0].PublicIPAddress.Id;
+
+                var ip = await Azure.PublicIPAddresses.GetByIdAsync(publicIPResourceId);
+
+                return ip.IPAddress;
+            }
+
+        }
     }
 }
